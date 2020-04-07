89bf6f5b8a13554f7f346247085e8a4c1a44c824
dilip640 <dilipsharma640@gmail.com>
2020-04-07 18:37:45 +0530

    Add department to adminview

--- course/admin.py
+++ course/admin.py
@@ -8,3 +8,8 @@ class CourseAdminView(admin.ModelAdmin):
     list_display = [field.name for field in models.Course._meta.fields]
 
 admin.site.register(models.Course, CourseAdminView)
+
+class DepartmentDeAdminView(admin.ModelAdmin):
+    list_display = [field.name for field in models.Department._meta.fields]
+
+admin.site.register(models.Department, DepartmentDeAdminView)
